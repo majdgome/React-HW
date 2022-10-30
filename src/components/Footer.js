544cc4cf1ed0b232e8a7b4676172c2aa3d70ba87
Amal-HF <amalis@outlook.sa>
2022-10-30 08:21:51 +0300

    final final final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,8 @@
 import React from 'react'
-// import satarLogo from '../Assests/satarLogo'
-// import sfcpdLogo from '../Assests/sfcpdLogo'
-// import img from '../Assests/stcLogo'
 
 function Footer() {
   return (
     <div className='footer'>
-        <div className='img-items'>
-            {/* <img src={mg}></img> */}
-        </div>
         <p>جميع الحقوق محفوظة للاتحاد السعودي للأمن السيبراني والبرمجة والدرونز 2021</p>
     </div>
   )
